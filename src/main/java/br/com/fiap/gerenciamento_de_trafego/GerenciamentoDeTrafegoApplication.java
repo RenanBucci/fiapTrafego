2aac285247e9e73f827afbefff01c226b4ba9871
Renan <renanbucci@gmail.com>
2024-10-18 16:37:06 -0300

    Docker funcionando corretamente

--- src/main/java/br/com/fiap/gerenciamento_de_trafego/GerenciamentoDeTrafegoApplication.java
+++ src/main/java/br/com/fiap/gerenciamento_de_trafego/GerenciamentoDeTrafegoApplication.java
@@ -2,9 +2,14 @@ package br.com.fiap.gerenciamento_de_trafego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "br.com.fiap.gerenciamento_de_trafego.repository")
+@EntityScan(basePackages = "br.com.fiap.gerenciamento_de_trafego.model")
 public class GerenciamentoDeTrafegoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciamentoDeTrafegoApplication.class, args);
